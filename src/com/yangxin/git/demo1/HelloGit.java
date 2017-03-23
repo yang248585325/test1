0f6cac16707f4dba3cc46a7f732aed317cf5231a
yang248585325 <248585325@qq.com>
2017-03-23 13:33:09 +0800

    测试

---
+++ src/com/yangxin/git/demo1/HelloGit.java
@@ -0,0 +1,8 @@
+package com.yangxin.git.demo1;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("hello git by yangxin");
+		System.out.println("hello git by yangxin");
+	}
+}
